319a949432339698f871da995b2e25066c6d36d5
tauri77 <mauriciogaletto@gmail.com>
2022-03-31 04:08:01 -0300

    Edit demo file

--- example/index.php
+++ example/index.php
@@ -5,114 +5,128 @@ require 'Flusher.php';
 require '../vendor/autoload.php';
 
 
-if ( isset( $_POST ) && isset( $_POST['action'] ) ) {
+if (isset($_POST) && isset($_POST['action'])) {
 
-	if ( $_POST['action'] === 'analyze' ) {
-		$downloader = new Tauri\M3u8Downloader\Downloader( $_POST['url'] );
-		$options    = $downloader->getSubList();
-		startDocument();
-		?>
-		<form action="" method="POST">
-			<label for="display_url">URL:</label> <span><?php echo htmlentities( $_POST['url'] ); ?></span>
-			<input type="hidden" name="url" id="url"
-				   value="<?php echo htmlspecialchars( $_POST['url'], ENT_COMPAT ); ?>"/>
-			<br>
-			<br>
-			<input type="hidden" name="action" value="download">
-			<?php
-			foreach ( $options as $option_key => $option ) {
-				echo "<label>";
-				echo "<input type='checkbox' name='subList[]' value='" . $option_key . "'>";
-				echo $option_key;
-				echo "</label><br>";
-			}
-			?>
-			<br>
-			<label>
-				<input type='checkbox' name='decrypt' value='1'>
-				Decrypt (if encrypted)
-			</label><br>
-			<br>
-			<label>
-				<input type='checkbox' name='join' value='1'>
-				Join segments
-			</label><br>
-			<br>
-			<label>
-				<input type='checkbox' id="isLive" name='isLive' value='1'>
-				Is Live
-			</label><br>
-			<div id="minutesContent" style="display:none">
-				<label for="minutes">Minutes to record:</label>
-				<input type="number" name="minutes" id="minutes" value="5">
-			</div>
-			<br>
-			<br>
-			<input type="submit" value="Download">
-		</form>
-		<script type="text/javascript">
-			document.getElementById("isLive").addEventListener('change', function (event) {
-				document.getElementById("minutesContent").style.display = event.currentTarget.checked ? 'block' : 'none';
-			})
-		</script>
-		<?php
-		endDocument();
-		die();
-	}
+    // Second view: Select the levels to download and settings (decrypt, join, live, etc..)
+    if ($_POST['action'] === 'analyze') {
+        // Instance downloader
+        $downloader = new Tauri\M3u8Downloader\Downloader($_POST['url']);
+        // Get the levels to list for select
+        $options = $downloader->getSubList();
 
-	if ( $_POST['action'] === 'download' ) {
+        startDocument();
 
-		ini_set( 'memory_limit', '1024M' );
-		set_time_limit( 60 * 60 * 6 );
-		ignore_user_abort( true );
+        //Create settings form
+        ?>
+        <form action="" method="POST">
+            <label for="display_url">URL:</label> <span><?php
+                echo htmlentities($_POST['url']); ?></span>
+            <input type="hidden" name="url" id="url"
+                   value="<?php
+                   echo htmlspecialchars($_POST['url'], ENT_COMPAT); ?>"/>
+            <br>
+            <br>
+            <input type="hidden" name="action" value="download">
+            <?php
+            foreach ($options as $option_key => $option) {
+                echo "<label>";
+                echo "<input type='checkbox' name='subList[]' value='" . $option_key . "'>";
+                echo $option_key;
+                echo "</label><br>";
+            }
+            ?>
+            <br>
+            <label>
+                <input type='checkbox' name='decrypt' value='1'>
+                Decrypt (if encrypted)
+            </label><br>
+            <br>
+            <label>
+                <input type='checkbox' name='join' value='1'>
+                Join segments
+            </label><br>
+            <br>
+            <label>
+                <input type='checkbox' id="isLive" name='isLive' value='1'>
+                Is Live
+            </label><br>
+            <div id="minutesContent" style="display:none">
+                <label for="minutes">Minutes to record:</label>
+                <input type="number" name="minutes" id="minutes" value="5">
+            </div>
+            <br>
+            <br>
+            <input type="submit" value="Download">
+        </form>
+        <script type="text/javascript">
+            document.getElementById("isLive").addEventListener('change', function (event) {
+                document.getElementById("minutesContent").style.display = event.currentTarget.checked ? 'block' : 'none';
+            })
+        </script>
+        <?php
 
-		//for send progress
-		$flusher = new Tauri\Flusher();
-		$flusher->setForFlush( 'text/html' );
+        endDocument();
+        die();
+    }
 
-		Tauri\M3u8Downloader\Downloader::$modeFile = 0777;
-        Tauri\M3u8Downloader\Downloader::$modeFile = 0777;
+    // Last view: Execute downloader
+    if ($_POST['action'] === 'download') {
 
-		$downloader = new Tauri\M3u8Downloader\Downloader(
-			$_POST['url'],
-			[
-				'saveTo' => './download_files/testing'.rand(0, 10000),
-				'decrypt' => ! empty( $_POST['decrypt'] ),
-				'joinSegments' => ! empty( $_POST['join'] ),
-			]
-		);
-		if ( ! empty( $_POST['subList'] ) ) {
-			$downloader->setSubList( $_POST['subList'] );
-		}
+        // Change limits
+        ini_set('memory_limit', '1024M');
+        set_time_limit(60 * 60 * 6);
+        ignore_user_abort(true);
 
-		$downloader->onProgress( function ( $p ) use ( $flusher ) {
-			$flusher->sendPercent( $p );
-		} );
+        // For send progress
+        $flusher = new Tauri\Flusher();
+        $flusher->setForFlush('text/html');
 
-		if ( ! empty( $_POST['isLive'] ) ) {
-			$downloader->downloadLive((int) $_POST['minutes']);
-		} else {
-			$downloader->download();
-		}
+        $downloader = new Tauri\M3u8Downloader\Downloader(
+            $_POST['url'],
+            [
+                'saveTo' => './download_files/testing' . rand(0, 10000),
+                'decrypt' => ! empty($_POST['decrypt']),
+                'joinSegments' => ! empty($_POST['join']),
+            ]
+        );
 
+        // Set the levels to download
+        if ( ! empty($_POST['subList'])) {
+            $downloader->setSubList($_POST['subList']);
+        }
+
+        // Show progress
+        $downloader->onProgress(function ($p) use ($flusher) {
+            $flusher->sendPercent($p);
+        });
+
+        // Initialize download
+        if ( ! empty($_POST['isLive'])) {
+            $downloader->downloadLive((int)$_POST['minutes']);
+        } else {
+            $downloader->download();
+        }
+
+        // Get the link to show
         $result = $downloader->saveFilePath();
-		$link = \Tauri\M3u8Downloader\HelperPath::relativeUrl( realpath(__FILE__), realpath( $result ) );
+        $link = \Tauri\M3u8Downloader\HelperPath::relativeUrl(realpath(__FILE__), realpath($result));
 
-		echo "<a href=\"". htmlspecialchars($link, ENT_COMPAT) .
-                "\">Downloaded m3u8 (". round($downloader->getDuration())." secs)</a>";
+        echo "<a href=\"" . htmlspecialchars($link, ENT_COMPAT) .
+            "\">Downloaded m3u8 (" . round($downloader->getDuration()) . " secs)</a>";
 
-		die();
-	}
-
+        die();
+    }
 }
+
+// Fist view
 startDocument();
 ?>
-	<form action="" method="POST">
-		<label for="url">URL:</label>
-		<input type="text" name="url" id="url"/>
-		<input type="hidden" name="action" value="analyze">
-		<input type="submit" value="Find levels">
-	</form>
+    <form action="" method="POST">
+        <label for="url">URL:</label>
+        <input type="text" name="url" id="url"/>
+        <input type="hidden" name="action" value="analyze">
+        <input type="submit" value="Find levels">
+    </form>
 <?php
 endDocument();
 
@@ -121,16 +135,17 @@ endDocument();
  *  HTML DOC PARTS
  *****************/
 function startDocument() {
-	?><!DOCTYPE html>
-	<html lang="EN">
-	<head>
-		<title>M3u8 Downloader Example</title>
-	</head>
-	<body>
-	<?php
+?><!DOCTYPE html>
+    <html lang="EN">
+    <head>
+        <title>M3u8 Downloader Example</title>
+    </head>
+    <body>
+    <?php
 }
 
-function endDocument() {
-	echo '	</body>
+function endDocument()
+{
+    echo '	</body>
 </html>';
 }
